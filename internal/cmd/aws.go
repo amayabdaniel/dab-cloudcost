1ba02ea51c59485f36a88344f95a989d82ab5cd1
Daniel <daniel.amaya.buitrago@outlook.com>
2025-12-03 11:08:18 -0500

    wire aws command to cost client

--- internal/cmd/aws.go
+++ internal/cmd/aws.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"text/tabwriter"
 
+	"github.com/amayabdaniel/dab-cloudcost/internal/aws"
 	"github.com/spf13/cobra"
 )
 
@@ -15,11 +19,38 @@ var awsCmd = &cobra.Command{
 	Use:   "aws",
 	Short: "Analyze AWS costs",
 	Long:  `Fetch and analyze costs from AWS Cost Explorer API.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Printf("fetching aws costs for last %d days using profile '%s'...\n", awsDays, awsProfile)
-		// TODO: implement cost fetching
+	RunE:  runAWS,
+}
+
+func runAWS(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+
+	fmt.Printf("fetching aws costs for last %d days...\n\n", awsDays)
+
+	client, err := aws.NewClient(ctx, awsProfile)
+	if err != nil {
+		return fmt.Errorf("failed to create aws client: %w", err)
+	}
+
+	costs, err := client.GetCostsByService(ctx, awsDays)
+	if err != nil {
+		return fmt.Errorf("failed to get costs: %w", err)
+	}
+
+	if len(costs) == 0 {
+		fmt.Println("no cost data found")
 		return nil
-	},
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+	fmt.Fprintln(w, "SERVICE\tCOST\tUNIT")
+	fmt.Fprintln(w, "-------\t----\t----")
+	for _, c := range costs {
+		fmt.Fprintf(w, "%s\t%s\t%s\n", c.Service, c.Amount, c.Unit)
+	}
+	w.Flush()
+
+	return nil
 }
 
 func init() {
